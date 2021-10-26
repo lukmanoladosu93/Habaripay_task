e3b430c3c11c1bb72ad776faf2a561941b8f8723
Lukman Oladosu <lukmanoladosu93@gmail.com>
2021-10-27 00:21:50 +0100

    Enhancements

--- src/app/features-section/features-section.component.ts
+++ src/app/features-section/features-section.component.ts
@@ -12,4 +12,15 @@ export class FeaturesSectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  copyLink(elementId:any){
+    let element = document.getElementById(elementId);
+    let elementText = element?.textContent!;
+
+    this.copyText(elementText);
+  }
+
+  copyText(text:string){
+    navigator.clipboard.writeText(text)
+  }
+
 }
